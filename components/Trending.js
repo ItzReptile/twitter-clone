a5145be519b9a3e0a65d5a42d23484797e9b5d43
jose <hernandezjos1121@gmail.com>
2023-09-06 01:21:38 -0400

    first

--- components/Trending.js
+++ components/Trending.js
@@ -26,25 +26,25 @@ export default function Trending() {
         <div className="p-3 relative">
           <DotsHorizontalIcon className="w-5 text-gray-600 absolute right-4" />
           <p className="text-xs text-gray-500">Trending In US</p>
-          <h1 className="text-[15px] font-bold">Donald Trump</h1>
+          <h1 className="text-[15px] font-bold">Seth Webster</h1>
           <p className="text-xs text-gray-500">154K Tweets</p>
         </div>
         <div className="p-3 relative">
           <DotsHorizontalIcon className="w-5 text-gray-600 absolute right-4" />
-          <p className="text-xs text-gray-500">Trending</p>
-          <h1 className="text-[15px] font-bold">Christians Bitch Ass</h1>
-          <p className="text-xs text-gray-500">1 Tweet</p>
+          <p className="text-xs text-gray-500">Take A Trip To?</p>
+          <h1 className="text-[15px] font-bold">Georgia</h1>
+          <p className="text-xs text-gray-500">125k Tweets</p>
         </div>
         <div className="p-3 relative">
           <DotsHorizontalIcon className="w-5 text-gray-600 absolute right-4" />
-          <p className="text-xs text-gray-500">Trending</p>
-          <h1 className="text-[15px] font-bold">Seth Websters Birthday</h1>
+          <p className="text-xs text-gray-500">Breaking News</p>
+          <h1 className="text-[15px] font-bold">Infinite</h1>
           <p className="text-xs text-gray-500">52K Tweets</p>
         </div>
         <div className="p-3 relative">
           <DotsHorizontalIcon className="w-5 text-gray-600 absolute right-4" />
-          <p className="text-xs text-gray-500">Trending</p>
-          <h1 className="text-[15px] font-bold">Lee Is A Bitch</h1>
+          <p className="text-xs text-gray-500">Would You Rather?</p>
+          <h1 className="text-[15px] font-bold">Leave A Nice House Or?</h1>
           <p className="text-xs text-gray-500">1.2M Tweets</p>
         </div>
       </div>
@@ -54,14 +54,14 @@ export default function Trending() {
           <div className=" flex space-x-3">
             <img
               className="w-11 h-11 object-cover rounded-full"
-              src="/assets/bragg.png"
+              src="https://scontent-ord5-2.xx.fbcdn.net/v/t1.18169-9/10402906_321736148273990_911058327573451551_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=WnTga17W2bEAX-FBDlK&_nc_ht=scontent-ord5-2.xx&oh=00_AfDN4GnpdOi5vQoKfN2_zL32ZP3cEmtE29Q8HSMyFHZx7Q&oe=651F7307"
             />
             <div>
               <div className="flex space-x-1">
-                <h1 className="font-bold">David Bragg</h1>
+                <h1 className="font-bold">Kelpo games</h1>
                 <BadgeCheckIcon className="w-[18px] text-blue-400" />
               </div>
-              <h1 className="text-[12px] text-gray-500 mt-1">@DavidBragg</h1>
+              <h1 className="text-[12px] text-gray-500 mt-1">@sirKelpo</h1>
             </div>
           </div>
           <button className="bg-white text-black text-sm w-20 rounded-3xl font-bold h-8">
@@ -72,14 +72,14 @@ export default function Trending() {
           <div className=" flex space-x-3">
             <img
               className="w-11 h-11 object-cover rounded-full"
-              src="/assets/bragg.png"
+              src="https://scontent-ord5-1.xx.fbcdn.net/v/t39.30808-6/315170633_1323457565136433_1047796810473003248_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=2p-pwhUiZgwAX9P0Q10&_nc_ht=scontent-ord5-1.xx&oh=00_AfDKg3TtsmoWYoD5OA3uBsXxk_TSCPWybWWR3Gu535BKqg&oe=64FCA811"
             />
             <div>
               <div className="flex space-x-1">
-                <h1 className="font-bold">David Bragg</h1>
+                <h1 className="font-bold">lee Martin</h1>
                 <BadgeCheckIcon className="w-[18px] text-blue-400" />
               </div>
-              <h1 className="text-[12px] text-gray-500 mt-1">@DavidBragg</h1>
+              <h1 className="text-[12px] text-gray-500 mt-1">@ElMartin</h1>
             </div>
           </div>
           <button className="bg-white text-black text-sm w-20 rounded-3xl font-bold h-8">
@@ -90,14 +90,14 @@ export default function Trending() {
           <div className=" flex space-x-3">
             <img
               className="w-11 h-11 object-cover rounded-full"
-              src="/assets/bragg.png"
+              src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/elden-ring/e/e5/20220307193717_1.jpg"
             />
             <div>
               <div className="flex space-x-1">
-                <h1 className="font-bold">David Bragg</h1>
+                <h1 className="font-bold">Reptile</h1>
                 <BadgeCheckIcon className="w-[18px] text-blue-400" />
               </div>
-              <h1 className="text-[12px] text-gray-500 mt-1">@DavidBragg</h1>
+              <h1 className="text-[12px] text-gray-500 mt-1">@ItzReptile</h1>
             </div>
           </div>
           <button className="bg-white text-black text-sm w-20 rounded-3xl font-bold h-8">
